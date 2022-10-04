9f2cd3c38eb0e7eb29b6974dece42315f9a6b1e1
gitsak12 <bhardwajsaket112@gmail.com>
2022-10-04 15:29:05 +0530

    commiting

---
+++ lib/icons.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+class CustomAppIcon {
+  CustomAppIcon._();
+
+  static const _kFontFam = 'CustomAppIcon';
+  static const String? _kFontPkg = null;
+
+  static const IconData chat = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData feed = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData groups = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData profile = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
