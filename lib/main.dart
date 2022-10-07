0c023b3e22a96e1ac37b0a6b6c92da3cc04a8c42
sohail2000 <msohail1379@gmail.com>
2022-10-08 00:45:43 +0530

    event_detail_api_int.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hillfair2022_frontend/screens/bottomnav/nav.dart';
-
+import 'package:hillfair2022_frontend/view_models/events_view_model.dart';
+import 'package:hillfair2022_frontend/view_models/team_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +13,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => EventsViewModel()),
+        ChangeNotifierProvider(create: (_) => TeamViewModel()),
+      ],
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         home: BottomNav(),
-      );
+      ),
+    );
   }
 }
